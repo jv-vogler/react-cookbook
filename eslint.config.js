def0243d8c6d3f1759d2913e600c83dc336d02f5
jv-vogler <jvsvogler@gmail.com>
2024-08-10 17:59:34 -0300

    chore(eslint): add parserOptions.project for type-aware rules

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,9 @@ export default tseslint.config({
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
+    parserOptions: {
+      project: ['./tsconfig.app.json'],
+    }
   },
   plugins: {
     'react-hooks': reactHooks,
